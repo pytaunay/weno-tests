3d95e2f96c7160309d0deddd4996cf81037abfb9
Pierre-Yves Taunay <5438710+pytaunay@users.noreply.github.com>
2018-11-20 13:00:54 -0500

    Implement flag to grab the correct flux

--- python/euler_1d/weno-euler.py
+++ python/euler_1d/weno-euler.py
@@ -8,7 +8,8 @@ Date: November 2018
 import numpy as np
 import matplotlib.pyplot as plt
 
-from utils import P_from_Ev, E_from_Pv
+from utils import P_from_Ev, rhoE_from_Pv
+from computeECUSPFlux import compute_ecusp_flux
 
 ###############
 #### SETUP ####
@@ -21,7 +22,7 @@ zvec = np.linspace(dz/2,L-dz/2,npt)
 
 # Time
 dt = dz / 1 * 0.4
-tmax = 0.14
+tmax = 2*dt
 tc = 0
 
 #Fluid
@@ -41,12 +42,12 @@ def f_0(U):
     b = (zvec < 0.5)
     U[:,0][b] = 1
     U[:,1][b] = 0
-    U[:,2][b] = E_from_Pv(1,U[:,0][b],U[:,1][b])
+    U[:,2][b] = rhoE_from_Pv(1,U[:,0][b],U[:,1][b])
     
     b = (zvec>=0.5)
     U[:,0][b] = 0.125
     U[:,1][b] = 0
-    U[:,2][b] = E_from_Pv(0.1,U[:,0][b],U[:,1][b])
+    U[:,2][b] = rhoE_from_Pv(0.1,U[:,0][b],U[:,1][b])
     
     
 f_0(U)
@@ -57,38 +58,44 @@ U0 = U
 #######################
 idx = 0
 
-def compute_flux_lf(uL,uR):
-    ### Left, right
-    fL = flux(uL)
-    fR = flux(uR)
-    
-    alpha = 1 # Derivative of flux
-    
-    return 1/2*(fL+fR-alpha*(uR-uL))    
+if flux_type == 'ECUSP':
+    compute_flux = lambda U,U0,dz,order: compute_ecusp_flux(U,U0,dz,order)
 
-def compute_flux_lw(uL,uR):
-    alpha = 1
-    u_lw = 1/2 * (uL+uR) - 1/2*alpha*(flux(uR)-flux(uL))
-    
-    return flux(u_lw)
-
-def compute_flux_force(uL,uR):
-    f_lf = compute_flux_lf(uL,uR)
-    f_lw = compute_flux_lw(uL,uR)
-    
-    return 1/2*(f_lf + f_lw)
+#def compute_flux_lf(uL,uR):
+#    ### Left, right
+#    fL = flux(uL)
+#    fR = flux(uR)
+#    
+#    alpha = 1 # Derivative of flux
+#    
+#    return 1/2*(fL+fR-alpha*(uR-uL))    
+#
+#def compute_flux_lw(uL,uR):
+#    alpha = 1
+#    u_lw = 1/2 * (uL+uR) - 1/2*alpha*(flux(uR)-flux(uL))
+#    
+#    return flux(u_lw)
+#
+#def compute_flux_force(uL,uR):
+#    f_lf = compute_flux_lf(uL,uR)
+#    f_lw = compute_flux_lw(uL,uR)
+#    
+#    return 1/2*(f_lf + f_lw)
     
 #
 while tc<tmax:
     UN = U  
-    U1 = UN + dt * compute_flux(UN)   
-    U2 = 3/4*UN + 1/4*U1 + 1/4* dt * compute_flux(U1) 
-    UNP1 = 1/3*UN + 2/3*U2 + 2/3 * dt * compute_flux(U2)
+#    UNP1 = UN + dt * compute_flux(UN,U0,dz,order)
+    U1 = UN + dt * compute_flux(UN,U0,dz,order)   
+    U2 = 3/4*UN + 1/4*U1 + 1/4* dt * compute_flux(U1,U0,dz,order) 
+    UNP1 = 1/3*UN + 2/3*U2 + 2/3 * dt * compute_flux(U2,U0,dz,order)
     
     U = UNP1
     
     tc = tc+dt
-#    
-#
+
+plt.plot(U,'o')
+plt.plot(U0,'k')
+
 #plt.plot(zvec,u0,'-')
 #plt.plot(zvec,uvec,'o')
