7d58976e9713b8256e3478787869226b0b4e46b3
Pierre-Yves Taunay <5438710+pytaunay@users.noreply.github.com>
2018-11-27 12:44:46 -0500

    Remove ECUSP from main file

--- python/euler_1d/weno-euler.py
+++ python/euler_1d/weno-euler.py
@@ -49,7 +49,7 @@ flux_type = 'LFC'
 U = np.zeros([len(zvec),3])
 
 # Case definition
-caseNum = 1
+caseNum = 7
 left, right, cfl, tmax = defineCase(caseNum)
 #tmax = 0.02
 f_0(U)
@@ -121,6 +121,7 @@ axarr[0,1].plot(zvec,P,'o')
 axarr[1,0].plot(zvec,P/U[:,0],'o')
 axarr[1,1].plot(zvec,U[:,1]/U[:,0],'o')
 axarr[2,0].plot(zvec,U[:,1]/U[:,0]/asos,'o')
+axarr[2,1].plot(zvec,U[:,2]/U[:,0],'o')
 
 ############### TRY WITH ECUSP
 U = np.copy(U0)
@@ -169,65 +170,12 @@ while tc<tmax:
     
     tc = tc+dt
 
-axarr[0,0].plot(zvec,U[:,0],'x')
-axarr[0,1].plot(zvec,P,'x')
-axarr[1,0].plot(zvec,P/U[:,0],'x')
-axarr[1,1].plot(zvec,U[:,1]/U[:,0],'x')
-axarr[2,0].plot(zvec,U[:,1]/U[:,0]/asos,'x')
-
-############### TRY WITH ECUSP
-U = np.copy(U0)
-tc = 0
-flux_type = 'ECUSP'
-dt = dz /lam0 * cfl
-idx = 0
-
-if flux_type == 'ECUSP':
-    compute_flux = lambda U,U0,dz,order: compute_ecusp_flux(U,U0,dz,order)
-elif flux_type == 'LF':
-    compute_flux = lambda U,U0,dz,order: compute_lf_flux(U,U0,dz,order)
-elif flux_type == 'LFC':
-    compute_flux = lambda U,U0,dz,order: compute_lfc_flux(U,U0,dz,order)
-
-while tc<tmax:
-    UN = np.copy(U)  
-#    UNP1 = UN + dt * compute_flux(UN,U0,dz,order)
-#    
-    U1 = UN + dt * compute_flux(UN,U0,dz,order)   
-    U1[0,:] = U0[0,:]
-    U1[-1,:] = U0[-1,:]
-    
-    U2 = 3/4*UN + 1/4*U1 + 1/4* dt * compute_flux(U1,U0,dz,order) 
-    U2[0,:] = U0[0,:]
-    U2[-1,:] = U0[-1,:]    
-    
-    UNP1 = 1/3*UN + 2/3*U2 + 2/3 * dt * compute_flux(U2,U0,dz,order)
-    UNP1[0,:] = U0[0,:]
-    UNP1[-1,:] = U0[-1,:] 
-    
-    U = np.copy(UNP1)
-
-    # Make sure we don't exceed the CFL condition by changing the time step
-    rho = U[:,0]
-    v = U[:,1] / rho
-    E = U[:,2] / rho
-    P = P_from_Ev(E,rho,v)
-    asos = np.sqrt(GAM * P / rho)
-    lam = np.max(np.abs(v)+asos)
-        
-    dt = cfl*dz/lam;
-    
-    if(tc + dt > tmax):
-        dt = tmax - tc
-    
-    tc = tc+dt
-
-axarr[0,0].plot(zvec,U[:,0],'s',mfc='none')
-axarr[0,1].plot(zvec,P,'s',mfc='none')
-axarr[1,0].plot(zvec,P/U[:,0],'s',mfc='none')
-axarr[1,1].plot(zvec,U[:,1]/U[:,0],'s',mfc='none')
-axarr[2,0].plot(zvec,U[:,1]/U[:,0]/asos,'s',mfc='none')
-
+axarr[0,0].plot(zvec,U[:,0],'-')
+axarr[0,1].plot(zvec,P,'-')
+axarr[1,0].plot(zvec,P/U[:,0],'-')
+axarr[1,1].plot(zvec,U[:,1]/U[:,0],'-')
+axarr[2,0].plot(zvec,U[:,1]/U[:,0]/asos,'-')
+axarr[2,1].plot(zvec,U[:,2]/U[:,0],'-')
 
 
 
