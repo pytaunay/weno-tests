b4e1a9b5e9a50889977f193ca5da4e02cd8ac9c6
Pierre-Yves Taunay <5438710+pytaunay@users.noreply.github.com>
2018-11-27 15:54:34 -0500

    Define a function to calculate dt

--- python/euler_2d/utils.py
+++ python/euler_2d/utils.py
@@ -5,6 +5,8 @@ Date: November 2018
 
 """
 
+import numpy as np
+
 GAM = 1.4
 
 # Useful functions
@@ -13,3 +15,40 @@ def P_from_Ev(E,rho,u,v):
     
 def rhoE_from_Pv(P,rho,u,v):
     return P/(GAM-1) + 1/2*rho*(u**2+v**2)
+
+def calculate_dt(U,dx,dy,cfl):
+    ### Calculate the physical quantities
+    rho = U[:,0]
+    u = U[:,1]/rho
+    v = U[:,2]/rho
+    E = U[:,3]/rho
+    
+    P = P_from_Ev(E,rho,u,v)    
+    a = np.sqrt(GAM*P/rho)
+    
+    ### Max eigenvalues
+    # x-direction
+    l1 = np.abs(u-a)   
+    l2 = np.abs(u)
+    l3 = np.abs(u+a)
+    
+    eig = np.vstack((l1,l2,l3))
+    
+    lx_max = np.max(eig)
+    
+    # y-direction
+    l1 = np.abs(v-a)   
+    l2 = np.abs(v)
+    l3 = np.abs(v+a)
+    
+    eig = np.vstack((l1,l2,l3))
+    
+    ly_max = np.max(eig)
+        
+    ### CFL condition
+    tx = 1/dx*lx_max
+    ty = 1/dy*ly_max
+    
+    dt = cfl/(tx+ty)
+
+    return dt
