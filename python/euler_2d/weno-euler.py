60dccd427762ed2dab29a24c21c044bb7ec5dfe3
Pierre-Yves Taunay <5438710+pytaunay@users.noreply.github.com>
2018-11-27 15:21:25 -0500

    Implement and test initialization

--- python/euler_2d/weno-euler.py
+++ python/euler_2d/weno-euler.py
@@ -9,32 +9,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from utils import P_from_Ev, rhoE_from_Pv
-from computeLFFlux import compute_lf_flux
 from computeLFCFlux import compute_lfc_flux
 from defineCase import defineCase
-
-# Initial conditions
-def f_0(U):
-    b = (zvec < 0.5)
-    U[:,0][b] = left[0]
-    U[:,1][b] = left[1] * left[0]
-    U[:,2][b] = rhoE_from_Pv(left[2],U[:,0][b],U[:,1][b]/U[:,0][b])
-    
-    b = (zvec>=0.5)
-    U[:,0][b] = right[0]
-    U[:,1][b] = right[1] * right[0]
-    U[:,2][b] = rhoE_from_Pv(right[2],U[:,0][b],U[:,1][b]/U[:,0][b])
-
+from initialize import initialize
 
 ###############
 #### SETUP ####
 ###############
-# Grid
-npt = 80
-L = 1
-dz = L/npt
-zvec = np.linspace(dz/2,L-dz/2,npt)
-
 # Fluid
 GAM = 1.4
 
@@ -43,25 +24,30 @@ GAM = 1.4
 order = 5
 flux_type = 'LFC'
 
-# Data holders
-# [rho,rho*u,E]
-U = np.zeros([len(zvec),3])
-
 # Case definition
-caseNum = 8
-left, right, cfl, tmax = defineCase(caseNum)
-#tmax = 0.02
-f_0(U)
-U0 = np.copy(U)
+case = 'double-mach'
+options = defineCase(case)
 
-# Time
-P0 = P_from_Ev(U[:,2]/U[:,0],U[:,0],U[:,1]/U[:,0])
-a0 = np.sqrt(GAM * P0 / U[:,0])
-v0 = U[:,1]/U[:,0]
-lam0 = np.max(v0+a0)
+U0,xvec,yvec,grid = initialize(options)
+xv, yv = np.meshgrid(xvec,yvec)
 
-dt = dz /lam0 * cfl
-tc = 0
+### Time
+tmax = options['tmax']
+cfl = options['cfl']
+
+
+# Resample the data on a mesh grid
+# dataZ = griddata(grid, U0[:,1], (xv, yv), method='nearest')
+
+
+## Time
+#P0 = P_from_Ev(U[:,2]/U[:,0],U[:,0],U[:,1]/U[:,0])
+#a0 = np.sqrt(GAM * P0 / U[:,0])
+#v0 = U[:,1]/U[:,0]
+#lam0 = np.max(v0+a0)
+#
+#dt = dz /lam0 * cfl
+#tc = 0
 
 # Plots
 f, axarr = plt.subplots(3, 2)
@@ -75,13 +61,7 @@ axarr[2,0].set_title("Mach number")
 #### TIME MARCHING ####
 #######################
 idx = 0
-
-if flux_type == 'ECUSP':
-    compute_flux = lambda U,U0,dz,order: compute_ecusp_flux(U,U0,dz,order)
-elif flux_type == 'LF':
-    compute_flux = lambda U,U0,dz,order: compute_lf_flux(U,U0,dz,order)
-elif flux_type == 'LFC':
-    compute_flux = lambda U,U0,dz,order: compute_lfc_flux(U,U0,dz,order)
+compute_flux = lambda U,U0,dz,order: compute_lfc_flux(U,U0,dz,order)
 
 while tc<tmax:
     UN = np.copy(U)  
@@ -122,81 +102,6 @@ axarr[1,1].plot(zvec,U[:,1]/U[:,0],'o')
 axarr[2,0].plot(zvec,U[:,1]/U[:,0]/asos,'o')
 axarr[2,1].plot(zvec,U[:,2]/U[:,0],'o')
 
-############### TRY WITH ECUSP
-U = np.copy(U0)
-tc = 0
-flux_type = 'LF'
-dt = dz /lam0 * cfl
-idx = 0
-
-if flux_type == 'ECUSP':
-    compute_flux = lambda U,U0,dz,order: compute_ecusp_flux(U,U0,dz,order)
-elif flux_type == 'LF':
-    compute_flux = lambda U,U0,dz,order: compute_lf_flux(U,U0,dz,order)
-elif flux_type == 'LFC':
-    compute_flux = lambda U,U0,dz,order: compute_lfc_flux(U,U0,dz,order)
-
-while tc<tmax:
-    UN = np.copy(U)  
-#    UNP1 = UN + dt * compute_flux(UN,U0,dz,order)
-#    
-    U1 = UN + dt * compute_flux(UN,U0,dz,order)   
-    U1[0,:] = U0[0,:]
-    U1[-1,:] = U0[-1,:]
-    
-    U2 = 3/4*UN + 1/4*U1 + 1/4* dt * compute_flux(U1,U0,dz,order) 
-    U2[0,:] = U0[0,:]
-    U2[-1,:] = U0[-1,:]    
-    
-    UNP1 = 1/3*UN + 2/3*U2 + 2/3 * dt * compute_flux(U2,U0,dz,order)
-    UNP1[0,:] = U0[0,:]
-    UNP1[-1,:] = U0[-1,:] 
-    
-    U = np.copy(UNP1)
-
-    # Make sure we don't exceed the CFL condition by changing the time step
-    rho = U[:,0]
-    v = U[:,1] / rho
-    E = U[:,2] / rho
-    P = P_from_Ev(E,rho,v)
-    asos = np.sqrt(GAM * P / rho)
-    lam = np.max(np.abs(v)+asos)
-        
-    dt = cfl*dz/lam;
-    
-    if(tc + dt > tmax):
-        dt = tmax - tc
-    
-    tc = tc+dt
-
-axarr[0,0].plot(zvec,U[:,0],'-')
-axarr[0,1].plot(zvec,P,'-')
-axarr[1,0].plot(zvec,P/U[:,0],'-')
-axarr[1,1].plot(zvec,U[:,1]/U[:,0],'-')
-axarr[2,0].plot(zvec,U[:,1]/U[:,0]/asos,'-')
-axarr[2,1].plot(zvec,U[:,2]/U[:,0],'-')
-
-
-
-
-
-# Exact solution
-caseStr = 'exact/' + str(caseNum) + '/case.csv'
-data = np.genfromtxt(caseStr, delimiter =',',names = True)
-#plt.plot(data['xe'],data['Me'],'k-')
-#plt.plot(data['xe'],data['rhoe'],'k-')
-
-axarr[0,0].plot(data['xe'],data['rhoe'],'k-')
-axarr[0,1].plot(data['xe'],data['pe'],'k-')
-axarr[1,0].plot(data['xe'],data['pe']/data['rhoe'],'k-')
-axarr[1,1].plot(data['xe'],data['ue'],'k-')
-axarr[2,0].plot(data['xe'],data['Me'],'k-')
-
-
-
-# MATLAB solution
-#qmatlab = np.genfromtxt('q.csv',delimiter=',')
-#plt.plot(zvec,qmatlab[:,0],'^')
 
 
 
