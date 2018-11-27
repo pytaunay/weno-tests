af8a3e90459d9ca9d40ef34abbfa7e21c9d71f6c
Pierre-Yves Taunay <5438710+pytaunay@users.noreply.github.com>
2018-11-27 15:21:08 -0500

    Add the second velocity component to the energy calculations

--- python/euler_2d/utils.py
+++ python/euler_2d/utils.py
@@ -8,8 +8,8 @@ Date: November 2018
 GAM = 1.4
 
 # Useful functions
-def P_from_Ev(E,rho,v):
-    return (GAM-1)*(rho*E-1/2*rho*v**2)
+def P_from_Ev(E,rho,u,v):
+    return (GAM-1)*(rho*E-1/2*rho*(u**2+v**2))
     
-def rhoE_from_Pv(P,rho,v):
-    return P/(GAM-1) + 1/2*rho*v**2
+def rhoE_from_Pv(P,rho,u,v):
+    return P/(GAM-1) + 1/2*rho*(u**2+v**2)
