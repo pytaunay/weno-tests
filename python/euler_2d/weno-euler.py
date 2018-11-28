a33da3563a1246d2bffcb43e8968c4e2e361389c
Pierre-Yves Taunay <5438710+pytaunay@users.noreply.github.com>
2018-11-28 18:43:04 -0500

    Add a few more lambda functions that depend on the case of interest

--- python/euler_2d/weno-euler.py
+++ python/euler_2d/weno-euler.py
@@ -8,10 +8,14 @@ Date: November 2018
 import numpy as np
 import matplotlib.pyplot as plt
 
-from utils import P_from_Ev, rhoE_from_Pv,calculate_dt
+from utils import P_from_Ev, rhoE_from_Pv, calculate_dt
 from computeLFCFlux import compute_lfc_flux
+from dmStencilCharacteristics import dmStencilCharacteristics
+from dmBoundaryCondition import dmBoundaryCondition
 from defineCase import defineCase
 from initialize import initialize
+
+from scipy.interpolate import griddata
 
 ###############
 #### SETUP ####
@@ -28,12 +32,15 @@ options = defineCase(case)
 
 U0,xvec,yvec,grid,dx,dy = initialize(options)
 xv, yv = np.meshgrid(xvec,yvec)
+Nx = len(xvec)
+Ny = len(yvec)
 
 ### Time
 tmax = options['tmax']
 cfl = options['cfl']
 dt = calculate_dt(U0,dx,dy,cfl)
 tc = 0
+tmax = 5*dt
 
 # Resample the data on a mesh grid
 # dataZ = griddata(grid, U0[:,1], (xv, yv), method='nearest')
@@ -50,46 +57,64 @@ axarr[2,0].set_title("Mach number")
 #### TIME MARCHING ####
 #######################
 idx = 0
-compute_flux = lambda U,U0,dz,order: compute_lfc_flux(U,U0,dz,order)
+lambda_calc_char = lambda U,flx,U0,flx0,order,Lh,alpha,direction,tc: dmStencilCharacteristics(U,flx,U0,flx0,order,Lh,alpha,Nx,Ny,direction,options,tc)
+compute_flux = lambda U,tc: compute_lfc_flux(U,U0,dx,dy,Nx,Ny,order,options,tc,lambda_calc_char)
+lambda_bc = lambda U,tc: dmBoundaryCondition(U,U0,options,tc)
 
+
+U = np.copy(U0)
+UNP1 = np.zeros(U0.shape)
+
+nelem = U.shape[0]
 while tc<tmax:
+    print(tc,dt)
     UN = np.copy(U)  
 
-    U1 = UN + dt * compute_flux(UN,U0,dz,order)  
-    U1[0,:] = U0[0,:]
-    U1[-1,:] = U0[-1,:]    
-    
-    U2 = 3/4*UN + 1/4*U1 + 1/4* dt * compute_flux(U1,U0,dz,order) 
-    U2[0,:] = U0[0,:]
-    U2[-1,:] = U0[-1,:]   
-
-    UNP1 = 1/3*UN + 2/3*U2 + 2/3 * dt * compute_flux(U2,U0,dz,order)
-    UNP1[0,:] = U0[0,:]
-    UNP1[-1,:] = U0[-1,:]   
-       
+    UNP1 = UN + dt * compute_flux(UN,tc)  
+#    U1 = UN + dt * compute_flux(UN,tc)  
+#    lambda_bc(U1,tc)
+##    
+#    U2 = 3/4*UN + 1/4*U1 + 1/4* dt * compute_flux(U1,tc)
+#    lambda_bc(U2,tc)
+#    
+#    UNP1 = 1/3*UN + 2/3*U2 + 2/3 * dt * compute_flux(U2,tc)
+#    lambda_bc(UNP1,tc)
+            
+    lambda_bc(UNP1,tc)
     U = np.copy(UNP1)
 
     # Make sure we don't exceed the CFL condition by changing the time step
     rho = U[:,0]
-    v = U[:,1] / U[:,0]
-    E = U[:,2] / U[:,0]
-    P = P_from_Ev(E,rho,v)
+    u = U[:,1] / U[:,0]
+    v = U[:,2] / U[:,0]
+    E = U[:,3] / U[:,0]
+    P = P_from_Ev(E,rho,u,v)
     asos = np.sqrt(GAM * P / rho)
     lam = np.max(np.abs(v)+asos)
         
-    dt = cfl*dz/lam;
-    
+    dt = calculate_dt(U,dx,dy,cfl)
+#    print(tc,dt)
     if(tc + dt > tmax):
         dt = tmax - tc
     
     tc = tc+dt
 
-axarr[0,0].plot(zvec,U[:,0],'o')
-axarr[0,1].plot(zvec,P,'o')
-axarr[1,0].plot(zvec,P/U[:,0],'o')
-axarr[1,1].plot(zvec,U[:,1]/U[:,0],'o')
-axarr[2,0].plot(zvec,U[:,1]/U[:,0]/asos,'o')
-axarr[2,1].plot(zvec,U[:,2]/U[:,0],'o')
+
+rhoZ = griddata(grid, rho, (xv, yv), method='nearest')
+uZ = griddata(grid, u, (xv, yv), method='nearest')
+vZ = griddata(grid, v, (xv, yv), method='nearest')
+EZ = griddata(grid, E, (xv, yv), method='nearest')
+PZ = griddata(grid, P, (xv, yv), method='nearest')
+TZ = PZ/rhoZ
+aZ = np.sqrt(GAM*PZ/rhoZ)
+MZ = np.sqrt(uZ**2+vZ**2)/aZ
+
+axarr[0,0].contourf(xv,yv,uZ)
+axarr[0,1].contourf(xv,yv,PZ)
+axarr[1,0].contourf(xv,yv,TZ)
+axarr[1,1].contourf(xv,yv,uZ)
+axarr[2,0].contourf(xv,yv,MZ)
+axarr[2,1].contourf(xv,yv,EZ)
 
 
 
