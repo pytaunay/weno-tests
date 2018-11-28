69d53fedcb6f782a6e828ea837495cc55dd7cd19
Pierre-Yves Taunay <5438710+pytaunay@users.noreply.github.com>
2018-11-28 18:42:50 -0500

    Split the characteristics decomposition into its own file with appropriate BC because it depends on the problem of interest

---
+++ python/euler_2d/dmStencilCharacteristics.py
@@ -0,0 +1,194 @@
+""" dnmtencilCharacteristics.py
+Calculates the stencil characteristics decomposition for all elements
+
+"""
+
+import numpy as np
+
+def dmStencilCharacteristics(U,flx,U0,flx0,order,Lh,alpha,Nx,Ny,direction,options,tc):
+    nelem = U.shape[0]
+    nunk = U.shape[1]    
+    rweno = (int)((order-1)/2)    
+    
+    # Matrix holders
+    V = np.zeros((nelem,order+1,nunk))
+    VLF = np.zeros((nelem,order+1,nunk))
+    H = np.zeros((nelem,order+1,nunk))    
+    
+    if direction == 'dx':
+        for idx in range(nelem):
+            if idx == 532:
+                print("532")
+            Utmp = np.zeros((order+1,nunk))
+            Ftmp = np.zeros((order+1,nunk))
+                
+            if order == 5:
+                if idx % Nx == 0: # Left boundary, 2 ghost cells --- inlet
+                    Utmp[0] = U0[idx]
+                    Utmp[1] = U0[idx]
+                    Ftmp[0] = flx0[idx]
+                    Ftmp[1] = flx0[idx]
+                    
+                    Utmp[2:6] = U[idx:idx+rweno+2]
+                    Ftmp[2:6] = flx[idx:idx+rweno+2]
+                    
+                elif idx % Nx == 1: # Left boundary, 1 ghost cell --- inlet
+                    Utmp[0] = U0[idx]
+                    Ftmp[0] = flx0[idx]
+                    
+                    Utmp[1:6] = U[idx-1:idx+rweno+2]
+                    Ftmp[1:6] = flx[idx-1:idx+rweno+2]
+                    
+                elif idx % Nx == Nx-3: # Right boundary, 1 ghost cell --- outlet Neumann
+                    Utmp[-1] = U[idx+2]
+#                    Ftmp[-1] = np.zeros((1,nunk))
+                    Ftmp[-1] = flx[idx+2]
+                    
+                    Utmp[0:5] = U[idx-rweno:idx+rweno+1]
+                    Ftmp[0:5] = flx[idx-rweno:idx+rweno+1]
+                    
+                elif idx % Nx == Nx-2: # Right boundary, 2 ghost cells --- outlet Neumann
+                    Utmp[-1] = U[idx+1]
+                    Utmp[-2] = U[idx+1]
+#                    Ftmp[-1] = np.zeros((1,nunk))
+#                    Ftmp[-2] = np.zeros((1,nunk))
+                    Ftmp[-1] = flx[idx+1] 
+                    Ftmp[-2] = flx[idx+1]
+                    
+                    Utmp[0:4] = U[idx-rweno:idx+rweno]
+                    Ftmp[0:4] = flx[idx-rweno:idx+rweno]
+                    
+                elif idx % Nx == Nx - 1: # Right boundary, 3 ghost cells --- outlet Neumann
+                    Utmp[-1] = U[idx]
+                    Utmp[-2] = U[idx]
+                    Utmp[-3] = U[idx]
+#                    Ftmp[-1] = np.zeros((1,nunk))
+#                    Ftmp[-2] = np.zeros((1,nunk))
+#                    Ftmp[-3] = np.zeros((1,nunk))
+                    Ftmp[-1] = flx[idx]
+                    Ftmp[-2] = flx[idx]
+                    Ftmp[-3] = flx[idx]
+                    
+                    Utmp[0:3] = U[idx-rweno:idx+rweno-1]
+                    Ftmp[0:3] = flx[idx-rweno:idx+rweno-1]
+                else: # Otherwise we are center of domain
+                    Utmp[0:6] = U[idx-rweno:idx+rweno+2]
+                    Ftmp[0:6] = flx[idx-rweno:idx+rweno+2]
+                    
+            # END IF ORDER == 5
+            # TODO: ORDER 3
+            # Perform the matrix multiplication
+            V[idx,:,:] = np.matmul(Lh[idx],Utmp.T).T
+            H[idx,:,:] = np.matmul(Lh[idx],Ftmp.T).T
+            VLF[idx,:,:] = np.matmul(np.diag(alpha),V[idx,:,:].T).T  
+        # END FOR NELEM LOOP
+    
+    elif direction == 'dy':
+        for idx in range(nelem):
+            Utmp = np.zeros((order+1,nunk))
+            Ftmp = np.zeros((order+1,nunk))
+            
+            yidx = (int)(idx/Nx) # Increases by 1 for every row
+            dx = options['xlim'][1] / options['grid'][0] 
+#            dy = options['ylim'][1] / options['grid'][1]
+            xc = dx/2 + (idx%Nx) * dx # Location of cell along x axis
+#            yc = dy/2 + yidx*dy
+            
+            # Current shock location
+            xs = options['xshock'] + (1+20*tc)/np.sqrt(3)
+            
+            if order == 5:
+                if yidx == 0: # Bottom boundary, 2 ghost cells
+                    if xc < options['xshock']: # If we are before the shock, enforce the upstream B.C.
+                        Utmp[0] = U0[0]
+                        Utmp[1] = U0[0]
+                        Ftmp[0] = flx0[0]
+                        Ftmp[1] = flx0[0]
+                    else:
+                        Utmp[0] = U[idx]
+                        Utmp[1] = U[idx]
+                        Ftmp[0] = flx[idx]
+                        Ftmp[1] = flx[idx]
+                    
+                    Utmp[2:6] = U[idx::Nx][0:4]
+                    Ftmp[2:6] = flx[idx::Nx][0:4]
+                    
+                elif yidx == 1: # Bottom boundary, 1 ghost cell
+                    if xc < options['xshock']: # If we are before the shock, enforce the upstream B.C.
+                        Utmp[0] = U0[0]
+                        Ftmp[0] = flx0[0]
+                    else:
+                        Utmp[0] = U[idx-yidx*Nx]
+                        Ftmp[0] = flx[idx-yidx*Nx]
+
+                    Utmp[1:6] = U[idx::Nx][0:5]
+                    Ftmp[1:6] = flx[idx::Nx][0:5]
+                    
+                elif yidx == Ny-3: # Top boundary, 1 ghost cell
+                    if xc < xs:
+                        Utmp[-1] = U0[0]
+                        Ftmp[-1] = flx0[0]
+                    else:
+                        Utmp[-1] = U0[-1] # Top right value. LIMITATION: CAN'T RUN PAST THE END OF DOMAIN
+                        Ftmp[-1] = flx0[-1]
+                    
+                    # Grab the bottom neighbors with idx::-Nx; limit to the last 5 with 0::5
+                    # Since ::-Nx inverts the list, put it back in correct order with ::-1
+                    # Example:
+                    # top row index = 100, Nx = 10, current idx = 80
+                    # idx + 2 *Nx::-10 = 100::-10 -> [100,90,80,70,60,50,40,30,20,10,0]
+                    # 0:5 -> [100,90,80,70,60]
+                    # ::-1 -> [60,70,80,90,100]
+                    Utmp[0:5] = U[idx+2*Nx::-Nx][0:5][::-1]
+                    Ftmp[0:5] = flx[idx+2*Nx::-Nx][0:5][::-1]
+                    
+                elif yidx == Ny-2: # Top boundary, 2 ghost cells
+                    if xc < xs:
+                        Utmp[-1] = U0[0]
+                        Utmp[-2] = U0[0]
+                        Ftmp[-1] = flx0[0]
+                        Ftmp[-2] = flx0[0]
+                    else:
+                        Utmp[-1] = U0[-1]
+                        Utmp[-2] = U0[-1]
+                        Ftmp[-1] = flx0[-1]
+                        Ftmp[-2] = flx0[-1]                        
+                    
+                    Utmp[0:4] = U[idx+Nx::-Nx][0:4][::-1]
+                    Ftmp[0:4] = flx[idx+Nx::-Nx][0:4][::-1]
+                    
+                elif yidx == Ny - 1: # Top boundary, 3 ghost cells
+                    if xc < xs:
+                        Utmp[-1] = U0[0]
+                        Utmp[-2] = U0[0]
+                        Utmp[-3] = U0[0]
+                        Ftmp[-1] = flx0[0]
+                        Ftmp[-2] = flx0[0]
+                        Ftmp[-3] = flx0[0]
+                    else:
+                        Utmp[-1] = U0[-1]
+                        Utmp[-2] = U0[-1]
+                        Utmp[-3] = U0[-1]
+                        Ftmp[-1] = flx0[-1]
+                        Ftmp[-2] = flx0[-1]
+                        Ftmp[-3] = flx0[-1]
+                    
+                    Utmp[0:3] = U[idx::-Nx][0:3][::-1]
+                    Ftmp[0:3] = flx[idx::-Nx][0:3][::-1]
+                else:
+                    Utmp[0:3] = U[idx::-Nx][0:3][::-1]
+                    Utmp[3:6] = U[idx+Nx::Nx][0:3]
+                    Ftmp[0:3] = flx[idx::-Nx][0:3][::-1]
+                    Ftmp[3:6] = flx[idx+Nx::Nx][0:3]
+                    
+                
+            # END IF ORDER == 5
+            # TODO: ORDER 3
+            
+            # Perform the matrix multiplication
+            V[idx,:,:] = np.matmul(Lh[idx],Utmp.T).T
+            H[idx,:,:] = np.matmul(Lh[idx],Ftmp.T).T
+            VLF[idx,:,:] = np.matmul(np.diag(alpha),V[idx,:,:].T).T    
+        # END FOR NELEM LOOP
+    # END DIRECTION CONDITIONAL
+    return V,H,VLF
--- python/euler_2d/eulerCharacteristics.py
+++ python/euler_2d/eulerCharacteristics.py
@@ -58,7 +58,7 @@ def eigenvector_x(u,v,a,q,h,nunk):
 
     # Left eigenvector
     Lj[0,0] = (GAM-1)*q + a*u
-    Lj[0,1] = (1-GAM)*u + a
+    Lj[0,1] = (1-GAM)*u - a
     Lj[0,2] = (1-GAM)*v
     Lj[0,3] = (GAM-1)
     Lj[0,:] /= (2*a**2)
@@ -73,7 +73,7 @@ def eigenvector_x(u,v,a,q,h,nunk):
     Lj[2,1] = (1-GAM)*u + a
     Lj[2,2] = (1-GAM)*v
     Lj[2,3] = (GAM-1)
-    Lj[2,:] /= 2*a*2
+    Lj[2,:] /= (2*a**2)
     
     Lj[3,0] = v
     Lj[3,2] = -1
@@ -118,7 +118,7 @@ def eigenvector_y(u,v,a,q,h,nunk):
     Lj[2,1] = (1-GAM)*u
     Lj[2,2] = (1-GAM)*v + a
     Lj[2,3] = (GAM-1)
-    Lj[2,:] /= 2*a*2
+    Lj[2,:] /= (2*a**2)
     
     Lj[3,0] = -u
     Lj[3,2] = 1
@@ -126,7 +126,7 @@ def eigenvector_y(u,v,a,q,h,nunk):
     return Rj, Lj    
 
 
-def compute_eigenvector(U,direction):
+def compute_eigenvector(U,U0,direction):
     rho = U[:,0]
     u = U[:,1] / rho
     v = U[:,2] / rho
@@ -140,17 +140,35 @@ def compute_eigenvector(U,direction):
     a = np.sqrt(GAM*P/rho)
     q = 1/2*(u**2 + v**2) # Dynamic pressure
     h = a**2/(GAM-1) + q # Enthalpy
+
+
+    rho0 = U[:,0]
+    u0 = U[:,1] / rho0
+    v0 = U[:,2] / rho0
+    E0 = U[:,3] / rho0
+    
+    P0 = P_from_Ev(E0,rho0,u0,v0)
+    
+    nunk = U0.shape[1]
+    nelem = U0.shape[0]
+    
+    a0 = np.sqrt(GAM*P0/rho0)
+    q0 = 1/2*(u0**2 + v0**2) # Dynamic pressure
+    h0 = a0**2/(GAM-1) + q0 # Enthalpy
+
     
     Rjlist = []
     Ljlist = []
     
     if direction == 'dx':
+        Rlhs0, Llhs0 = eigenvector_x(u0[0],v0[0],a0[0],q0[0],h0[0],nunk)
         for idx in range(nelem):
             Rj, Lj = eigenvector_x(u[idx],v[idx],a[idx],q[idx],h[idx],nunk)
             Rjlist.append(Rj)
             Ljlist.append(Lj)
     
     elif direction == 'dy':
+        Rlhs0, Llhs0 = eigenvector_y(u0[0],v0[0],a0[0],q0[0],h0[0],nunk)
         for idx in range(nelem):
             Rj, Lj = eigenvector_y(u[idx],v[idx],a[idx],q[idx],h[idx],nunk)
             Rjlist.append(Rj)
@@ -159,13 +177,12 @@ def compute_eigenvector(U,direction):
     Rj = Rjlist
     Lj = Ljlist
 
-    return Rj,Lj     
+    return Rj,Lj,Rlhs0,Llhs0
  
 
-def to_characteristics(U,flx,U0,flx0,order,Lh,alpha,Nx,Ny,direction):
+def to_characteristics(U,flx,U0,flx0,order,Lh,alpha,Nx,Ny,direction,options,tc,lambda_calc_char):
     nelem = U.shape[0]
     nunk = U.shape[1]    
-    rweno = (int)((order-1)/2)    
     
     # Matrix holders
     V = np.zeros((nelem,order+1,nunk))
@@ -173,141 +190,10 @@ def to_characteristics(U,flx,U0,flx0,order,Lh,alpha,Nx,Ny,direction):
     H = np.zeros((nelem,order+1,nunk))
     
     # For all elements, evaluate R_{i+1/2}^-1 * [STENCIL]   
-    # The conditional work for r = 1 and 2 (order 3 and 5). NOT HIGHER
+    # The conditional work for r = 2 (order 5)
+    # We do the characteristics calculation for all elements for the whole stencil
+    V,H,VLF = lambda_calc_char(U,flx,U0,flx0,order,Lh,alpha,direction,tc)
     
-    if direction == 'dx':
-        for idx in range(nelem):
-            Utmp = np.zeros((order+1,nunk))
-            Ftmp = np.zeros((order+1,nunk))
-                
-            if order == 5:
-                if idx % Nx == 0: # Left boundary, 2 ghost cells --- inlet
-                    Utmp[0] = U0[idx]
-                    Utmp[1] = U0[idx]
-                    Ftmp[0] = flx0[idx]
-                    Ftmp[1] = flx0[idx]
-                    
-                    Utmp[2:6] = U[idx:idx+rweno+2]
-                    Ftmp[2:6] = flx[idx:idx+rweno+2]
-                    
-                elif idx % Nx == 1: # Left boundary, 1 ghost cell --- inlet
-                    Utmp[0] = U0[idx]
-                    Ftmp[0] = flx0[idx]
-                    
-                    Utmp[1:6] = U[idx-1:idx+rweno+2]
-                    Ftmp[1:6] = flx[idx-1:idx+rweno+2]
-                    
-                elif idx % Nx == Nx-3: # Right boundary, 1 ghost cell --- outlet Neumann
-                    Utmp[-1] = U[idx]
-                    Ftmp[-1] = np.zeros((1,nunk))
-                    
-                    Utmp[0:5] = U[idx-rweno:idx+rweno+1]
-                    Ftmp[0:5] = flx[idx-rweno:idx+rweno+1]
-                    
-                elif idx % Nx == Nx-2: # Right boundary, 2 ghost cells --- outlet Neumann
-                    Utmp[-1] = U[idx]
-                    Utmp[-2] = U[idx]
-                    Ftmp[-1] = np.zeros((1,nunk))
-                    Ftmp[-2] = np.zeros((1,nunk))
-                    
-                    Utmp[0:4] = U[idx-rweno:idx+rweno]
-                    Ftmp[0:4] = flx[idx-rweno:idx+rweno]
-                    
-                elif idx % Nx == Nx - 1: # Right boundary, 3 ghost cells --- outlet Neumann
-                    Utmp[-1] = U[idx]
-                    Utmp[-2] = U[idx]
-                    Utmp[-3] = U[idx]
-                    Ftmp[-1] = np.zeros((1,nunk))
-                    Ftmp[-2] = np.zeros((1,nunk))
-                    Ftmp[-3] = np.zeros((1,nunk))
-                    
-                    Utmp[0:3] = U[idx-rweno:idx+rweno-1]
-                    Ftmp[0:3] = flx[idx-rweno:idx+rweno-1]
-                else: # Otherwise we are center of domain
-                    Utmp[0:6] = U[idx-rweno:idx+rweno+2]
-                    Ftmp[0:6] = flx[idx-rweno:idx+rweno+2]
-                    
-            # END IF ORDER == 5
-            # TODO: ORDER 3
-            # Perform the matrix multiplication
-            V[idx,:,:] = np.matmul(Lh[idx],Utmp.T).T
-            H[idx,:,:] = np.matmul(Lh[idx],Ftmp.T).T
-            VLF[idx,:,:] = np.matmul(np.diag(alpha),V[idx,:,:].T).T  
-        # END FOR NELEM LOOP
-    
-    elif direction == 'dy':
-        for idx in range(nelem):
-            Utmp = np.zeros((order+1,nunk))
-            Ftmp = np.zeros((order+1,nunk))
-            
-            yidx = (int)(idx/Nx) # Increases by 1 for every row
-            
-            if order == 5:
-                if yidx == 0: # Bottom boundary, 2 ghost cells --- reflective
-                    Utmp[0] = U[idx]
-                    Utmp[1] = U[idx]
-                    Ftmp[0] = np.zeros((1,nunk))
-                    Ftmp[1] = np.zeros((1,nunk))
-                    
-                    Utmp[2:6] = U[idx::Nx][0:4]
-                    Ftmp[2:6] = flx[idx::Nx][0:4]
-                    
-                elif yidx == 1: # Bottom boundary, 1 ghost cell --- reflective
-                    Utmp[0] = U[idx]
-                    Ftmp[0] = np.zeros((1,nunk))
-                    
-                    Utmp[1:6] = U[idx::Nx][0:5]
-                    Ftmp[1:6] = flx[idx::Nx][0:5]
-                    
-                elif yidx == Ny-3: # Top boundary, 1 ghost cell --- reflective
-                    Utmp[-1] = U[idx + 2*Nx]
-                    Ftmp[-1] = np.zeros((1,nunk))
-                    
-                    # Grab the bottom neighbors with idx::-Nx; limit to the last 5 with 0::5
-                    # Since ::-Nx inverts the list, put it back in correct order with ::-1
-                    # Example:
-                    # top row index = 100, Nx = 10, current idx = 80
-                    # idx + 2 *Nx::-10 = 100::-10 -> [100,90,80,70,60,50,40,30,20,10,0]
-                    # 0:5 -> [100,90,80,70,60]
-                    # ::-1 -> [60,70,80,90,100]
-                    Utmp[0:5] = U[idx+2*Nx::-Nx][0:5][::-1]
-                    Ftmp[0:5] = flx[idx+2*Nx::-Nx][0:5][::-1]
-                    
-                elif yidx == Ny-2: # Top boundary, 2 ghost cells --- reflective
-                    Utmp[-1] = U[idx+Nx]
-                    Utmp[-2] = U[idx+Nx]
-                    Ftmp[-1] = np.zeros((1,nunk))
-                    Ftmp[-2] = np.zeros((1,nunk))
-                    
-                    Utmp[0:4] = U[idx+Nx::-Nx][0:4][::-1]
-                    Ftmp[0:4] = flx[idx+Nx::-Nx][0:4][::-1]
-                    
-                elif yidx == Ny - 1: # Top boundary, 3 ghost cells --- reflective
-                    Utmp[-1] = U[idx]
-                    Utmp[-2] = U[idx]
-                    Utmp[-3] = U[idx]
-                    Ftmp[-1] = np.zeros((1,nunk))
-                    Ftmp[-2] = np.zeros((1,nunk))
-                    Ftmp[-3] = np.zeros((1,nunk))
-                    
-                    Utmp[0:3] = U[idx::-Nx][0:3][::-1]
-                    Ftmp[0:3] = flx[idx::-Nx][0:3][::-1]
-                else:
-                    Utmp[0:3] = U[idx::-Nx][0:3][::-1]
-                    Utmp[3:6] = U[idx+Nx::Nx][0:3]
-                    Ftmp[0:3] = flx[idx::-Nx][0:3][::-1]
-                    Ftmp[3:6] = flx[idx+Nx::Nx][0:3]
-                    
-                
-            # END IF ORDER == 5
-            # TODO: ORDER 3
-            
-            # Perform the matrix multiplication
-            V[idx,:,:] = np.matmul(Lh[idx],Utmp.T).T
-            H[idx,:,:] = np.matmul(Lh[idx],Ftmp.T).T
-            VLF[idx,:,:] = np.matmul(np.diag(alpha),V[idx,:,:].T).T    
-        # END FOR NELEM LOOP
-    # END DIRECTION CONDITIONAL
 
 
     return V,H,VLF
