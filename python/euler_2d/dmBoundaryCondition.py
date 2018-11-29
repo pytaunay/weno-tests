a0ed6916e60ab93d3c8d3fd986b87c15712a75d1
Pierre-Yves Taunay <5438710+pytaunay@users.noreply.github.com>
2018-11-29 16:27:04 -0500

    Edit BC for the double Mach problem

--- python/euler_2d/dmBoundaryCondition.py
+++ python/euler_2d/dmBoundaryCondition.py
@@ -5,24 +5,28 @@ Imposes the boundary conditions for the double Mach problem
 import numpy as np
 
 def dmBoundaryCondition(U,U0,options,tc):
-    nelem = U.shape[0]
-    Nx = options['grid'][0]
-    Ny = options['grid'][1]
+    Nx = options['grid'][0] + 2
     
     dx = options['xlim'][1] / options['grid'][0] 
     xs = options['xshock'] + (1+20*tc)/np.sqrt(3) # Shock location
     
-    for idx in range(nelem):
-        yidx = (int)(idx/Nx) # Increases by 1 for every row
-        xc = dx + (idx%Nx) * dx
-        
-        if idx % Nx == 0: # Left boundary
+    U[::Nx] = options['Upost'] # Left boundary
+    U[Nx-1::Nx] = options['Upre'] # Right boundary
+    
+    
+    for idx in range(1,Nx-1):
+        xc = (idx % Nx) * dx
+        # Bottom boundary
+        if xc <= options['xshock']: # Past the shock (upstream)
             U[idx] = options['Upost']
-        elif yidx == 0: # Bottom boundary
-            if xc < options['xshock']:
-                U[idx] = options['Upost']
-        elif yidx == Ny-1: # Top boundary
-            if xc < xs:
-                U[idx] = options['Upost']
-            else:
-                U[idx] = options['Upre']
+        else: # After the shock (downstream): reflective wall
+            U[idx,0] = U[idx+Nx,0]    
+            U[idx,1] = U[idx+Nx,1] 
+            U[idx,3] = U[idx+Nx,3] 
+
+        # Top boundary               
+        if xc < xs:
+            U[idx] = options['Upost']
+        else:
+            U[idx] = options['Upre']
+                
