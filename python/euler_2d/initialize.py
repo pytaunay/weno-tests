2029c84384b542f278acfd880e33408a8058453e
Pierre-Yves Taunay <5438710+pytaunay@users.noreply.github.com>
2018-11-29 16:27:34 -0500

    Fix Nx and Ny definition

--- python/euler_2d/initialize.py
+++ python/euler_2d/initialize.py
@@ -17,40 +17,43 @@ def initialize_dm(options):
     Nx = options['grid'][0]
     Ny = options['grid'][1]
     
+    Nxtot = Nx + 2
+    Nytot = Ny + 2
+    
     # Total number of points WITH the boundaries included
     # The boundaries are at x = 0, y = 0, x = L, y = L
-    nelem = (Nx+2)*(Ny+2)
+    nelem = Nxtot * Nytot
     nunk = options['Upost'].shape[0] # Number of unknowns
     
     Lx = options['xlim'][1]-options['xlim'][0]
     Ly = options['ylim'][1]-options['ylim'][0]
     
-    dx = Lx/(Nx+1)
-    dy = Ly/(Ny+1)
+    dx = Lx/(Nxtot-1)
+    dy = Ly/(Nytot-1)
     
     #xvec = np.linspace(dx/2,Lx-dx/2,Nx)
     #yvec = np.linspace(dy/2,Ly-dy/2,Ny)
-    xvec = np.linspace(0,Lx,Nx+2)
-    yvec = np.linspace(0,Ly,Ny+2)
+    xvec = np.linspace(0,Lx,Nxtot)
+    yvec = np.linspace(0,Ly,Nytot)
 
     U = np.zeros((nelem,nunk))
     grid = np.zeros((nelem,2))
     
-    for jj in range(Ny):
+    for jj in range(Nytot):
         yc = yvec[jj]
-        for ii in range(Nx):
+        for ii in range(Nxtot):
             xc = xvec[ii]
             
             slope = np.tan(options['angleshock'])
             ylim = slope*(xc - options['xshock'])
             
             if xc <= options['xshock']:
-                U[ii+Nx*jj] = options['Upost']
+                U[ii+Nxtot*jj] = options['Upost']
             elif xc > options['xshock'] and yc >= ylim:
-                U[ii+Nx*jj] = options['Upost']
+                U[ii+Nxtot*jj] = options['Upost']
             else:
-                U[ii+Nx*jj] = options['Upre']
+                U[ii+Nxtot*jj] = options['Upre']
              
-            grid[ii+Nx*jj] = np.array([xc,yc])    
+            grid[ii+Nxtot*jj] = np.array([xc,yc])    
                 
     return U,xvec,yvec,grid,dx,dy
