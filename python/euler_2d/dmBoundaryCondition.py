f6ba29c3532d5ab7cc11e5f4789403a6f192d95f
Pierre-Yves Taunay <5438710+pytaunay@users.noreply.github.com>
2018-11-29 18:27:22 -0500

    Fix boundary condition for domain

--- python/euler_2d/dmBoundaryCondition.py
+++ python/euler_2d/dmBoundaryCondition.py
@@ -6,27 +6,29 @@ import numpy as np
 
 def dmBoundaryCondition(U,U0,options,tc):
     Nx = options['grid'][0] + 2
+    Ny = options['grid'][1] + 2
     
-    dx = options['xlim'][1] / options['grid'][0] 
+    dx = options['xlim'][1] / Nx
     xs = options['xshock'] + (1+20*tc)/np.sqrt(3) # Shock location
     
     U[::Nx] = options['Upost'] # Left boundary
     U[Nx-1::Nx] = options['Upre'] # Right boundary
     
-    
     for idx in range(1,Nx-1):
-        xc = (idx % Nx) * dx
+        xc = idx * dx
+        
         # Bottom boundary
-        if xc <= options['xshock']: # Past the shock (upstream)
+        if xc < options['xshock']: # Past the shock (upstream)
             U[idx] = options['Upost']
         else: # After the shock (downstream): reflective wall
             U[idx,0] = U[idx+Nx,0]    
             U[idx,1] = U[idx+Nx,1] 
+            U[idx,2] = 0
             U[idx,3] = U[idx+Nx,3] 
 
         # Top boundary               
         if xc < xs:
-            U[idx] = options['Upost']
+            U[Nx*(Ny-1)+idx] = options['Upost']
         else:
-            U[idx] = options['Upre']
+            U[Nx*(Ny-1)+idx] = options['Upre']
                 
