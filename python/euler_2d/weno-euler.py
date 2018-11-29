beaf25e649af9f586da46a24b2bcd706db6ed7de
Pierre-Yves Taunay <5438710+pytaunay@users.noreply.github.com>
2018-11-29 18:43:27 -0500

    Change plotting - it still needs fixing

--- python/euler_2d/weno-euler.py
+++ python/euler_2d/weno-euler.py
@@ -40,7 +40,7 @@ tmax = options['tmax']
 cfl = options['cfl']
 dt = calculate_dt(U0,dx,dy,cfl)
 tc = 0
-tmax = 0
+tmax = 0.05
 
 # Resample the data on a mesh grid
 # dataZ = griddata(grid, U0[:,1], (xv, yv), method='nearest')
@@ -50,8 +50,34 @@ f, axarr = plt.subplots(3, 2)
 axarr[0,0].set_title("Density")
 axarr[0,1].set_title("Pressure")
 axarr[1,0].set_title("Temperature")
-axarr[1,1].set_title("Velocity")
+axarr[1,1].set_title("x-Velocity")
 axarr[2,0].set_title("Mach number")
+axarr[2,1].set_title("Energy")
+
+rho = U0[:,0]
+u = U0[:,1] / U0[:,0]
+v = U0[:,2] / U0[:,0]
+E = U0[:,3] / U0[:,0]
+P = P_from_Ev(E,rho,u,v)
+asos = np.sqrt(GAM * P / rho)
+lam = np.max(np.abs(v)+asos)
+        
+rhoZ = griddata(grid, rho, (xv, yv), method='nearest')
+uZ = griddata(grid, u, (xv, yv), method='nearest')
+vZ = griddata(grid, v, (xv, yv), method='nearest')
+EZ = griddata(grid, E, (xv, yv), method='nearest')
+PZ = griddata(grid, P, (xv, yv), method='nearest')
+TZ = PZ/rhoZ
+aZ = np.sqrt(GAM*PZ/rhoZ)
+MZ = np.sqrt(uZ**2+vZ**2)/aZ
+
+p0 = axarr[0,0].contour(xv,yv,rhoZ)
+p1 = axarr[0,1].contour(xv,yv,PZ)
+p2 = axarr[1,0].contour(xv,yv,TZ)
+p3 = axarr[1,1].contour(xv,yv,uZ)
+p4 = axarr[2,0].contour(xv,yv,MZ)
+p5 = axarr[2,1].contour(xv,yv,EZ)
+
 
 #######################
 #### TIME MARCHING ####
@@ -92,7 +118,25 @@ while tc<tmax:
     asos = np.sqrt(GAM * P / rho)
     lam = np.max(np.abs(v)+asos)
         
+    rhoZ = griddata(grid, rho, (xv, yv), method='nearest')
+    uZ = griddata(grid, u, (xv, yv), method='nearest')
+    vZ = griddata(grid, v, (xv, yv), method='nearest')
+    EZ = griddata(grid, E, (xv, yv), method='nearest')
+    PZ = griddata(grid, P, (xv, yv), method='nearest')
+    TZ = PZ/rhoZ
+    aZ = np.sqrt(GAM*PZ/rhoZ)
+    MZ = np.sqrt(uZ**2+vZ**2)/aZ
     
+    axarr[0,0].contour(xv,yv,uZ)
+    axarr[0,1].contour(xv,yv,PZ)
+    axarr[1,0].contour(xv,yv,TZ)
+    axarr[1,1].contour(xv,yv,uZ)
+    axarr[2,0].contour(xv,yv,MZ)
+    axarr[2,1].contour(xv,yv,EZ)
+    
+    f.canvas.draw()
+    f.canvas.flush_events()
+
     
 #    dt = calculate_dt(U,dx,dy,cfl)
 #    print(tc,dt)
@@ -102,21 +146,8 @@ while tc<tmax:
     tc = tc+dt
 
 
-rhoZ = griddata(grid, rho, (xv, yv), method='nearest')
-uZ = griddata(grid, u, (xv, yv), method='nearest')
-vZ = griddata(grid, v, (xv, yv), method='nearest')
-EZ = griddata(grid, E, (xv, yv), method='nearest')
-PZ = griddata(grid, P, (xv, yv), method='nearest')
-TZ = PZ/rhoZ
-aZ = np.sqrt(GAM*PZ/rhoZ)
-MZ = np.sqrt(uZ**2+vZ**2)/aZ
 
-axarr[0,0].contour(xv,yv,uZ)
-axarr[0,1].contour(xv,yv,PZ)
-axarr[1,0].contour(xv,yv,TZ)
-axarr[1,1].contour(xv,yv,uZ)
-axarr[2,0].contour(xv,yv,MZ)
-axarr[2,1].contour(xv,yv,EZ)
+
 
 
 
